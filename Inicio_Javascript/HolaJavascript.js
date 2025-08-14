822a21eed136e64424c80bd20cb1540af4f626d9
Thibor82 <ruben.caballero.lopez@gmail.com>
2025-08-14 12:14:41 +0200

    Iniciamos Javascript

---
+++ Inicio_Javascript/HolaJavascript.js
@@ -0,0 +1,25 @@
+
+
+//variables y constantes//
+var y = 1;
+let x = 2;
+const z = 3;
+
+//declarar variable//
+
+let a;
+
+//inicializar variable//
+//1 declarar e inicializar a la vez//
+let b = 1;
+//2 una variable ya declarada se le da un valor//
+a=1;
+
+
+//Tipos de datos//
+let entero = 1;
+let decimal = 2.67;
+let texto = "Hola"; //dato de tipo string, o cadena de caracteres//
+let textonum = "1";
+let booleano = true; //or false//
+
